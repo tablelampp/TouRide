f6f7d62dd1c67af483d1827e7e1e842f54791642
Yonas Asmelash <yonasasmelashy@gmail.com>
2024-12-06 20:07:31 -0500

    HTTP Route created + Databse (MongoDB and mongoose) added to Node.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://yonasasmelash:tablelamp@touridecluster.1lne5.mongodb.net/idk-collection?retryWrites=true&w=majority&appName=TouRideCluster';
